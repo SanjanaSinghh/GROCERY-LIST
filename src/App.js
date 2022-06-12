59eb47a63adf1d63661f98f185ce2324784c1131
Sanjana Kumari Singh <89686839+SanjanaSinghh@users.noreply.github.com>
2022-06-12 15:06:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <>
       <div className="App">
-        <h1>S&D Shah</h1>
+        <h1>Grocery</h1>
         {/* <h2>Start editing to see some magic happen!</h2> */}
         <List />
       </div>
